23dd8fb7dbf50daac40510f4c30e5e7d48bf4c37
justsixthree <bhsony2@gmail.com>
2018-05-08 20:27:19 +0530

    new _ nus

---
+++ new_nuscriptllc/assets/new_js/header-footer.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+var footer;
+        footer = `<footer class="footer">
+            <div class="container-fluid">
+                <div class="container-margin">
+                    <ul class="footer-menu"><li class="menu-662 first"><a href="about.html" title="">About</a></li>
+                        <li><a href="services-business.html" title="">Services</a></li>
+                        <li><a href="our-advantage" title="">NuScript Advantage</a></li>
+                        <li><a href="#" title="">Careers</a></li>
+                        <li><a href="#" title="" class="active-trail active">Privacy Policy</a></li>
+                        <li><a href="#" title="">Client</a></li>
+                        <li><a href="#" title="">Login</a></li>
+                        <li><a href="contact.html" title="">Contact</a></li>
+                    </ul>
+                    <span class="footer-copyright"><a href="home" class="copyright">© 2018 NuScript LLC</a></span>
+                    <div class="footer-social">
+                        <ul class="footer-social__list social-media">
+                            <li><a href="" ><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="" ><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="" ><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="" ><i class="fa fa-google"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+`;
+    
+    
+    $('.site-footer').html(footer);
+    
+    
+        });
+
+
